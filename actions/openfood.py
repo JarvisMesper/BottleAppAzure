fa6a7e565530eef868d456d5e56488a73b1b69c8
nathanquinteiro <nathan.quinteiro@gmail.com>
2017-04-23 19:22:47 +0200

    test

--- actions/openfood.py
+++ actions/openfood.py
@@ -13,7 +13,7 @@ def getOpenFoodInfo(request):
         try:
             res = RequestOpenFood.get_product(barcode=product)
             res = ProductBuilder.clean_data(res)
-            context['info'] = res[0:10]
+            context['info'] = "sfdlkajfljéewekj"[0:10]
             if context.get('missing_id') is not None:
                 del context['missing_id']
         except:
