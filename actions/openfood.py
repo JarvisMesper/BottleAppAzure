82a4af4abe595ee33ec74ef52430303cb8aaa847
nathanquinteiro <nathan.quinteiro@gmail.com>
2017-04-23 19:05:20 +0200

    ..

--- actions/openfood.py
+++ actions/openfood.py
@@ -7,11 +7,20 @@ def getOpenFoodInfo(request):
     context = request['context']
     entities = request['entities']
 
-    if context:
-        print('context : ', context)
-    if entities:
-        print('entities : ', entities)
+    product = wit.first_entity_value(entities, 'product_id')
+    if product:
 
-
+        try:
+            res = RequestOpenFood.get_product(barcode=product)
+            #res = ProductBuilder.clean_data(res)
+            context['info'] = res
+            if context.get('missing_id') is not None:
+                del context['missing_id']
+        except:
+            context['info'] = "Can't find this product on the OpenFood database"
+    else:
+        context['missing_id'] = True
+        if context.get('info') is not None:
+            del context['info']
 
     return context
