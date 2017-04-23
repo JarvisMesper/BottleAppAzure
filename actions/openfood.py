640efffc85e8ccc1f6d120e93740c99dba2d0f99
nathanquinteiro <nathan.quinteiro@gmail.com>
2017-04-23 19:09:20 +0200

    limit answer size

--- actions/openfood.py
+++ actions/openfood.py
@@ -13,7 +13,7 @@ def getOpenFoodInfo(request):
         try:
             res = RequestOpenFood.get_product(barcode=product)
             #res = ProductBuilder.clean_data(res)
-            context['info'] = res
+            context['info'] = "I have found result about this product but can't write it here."
             if context.get('missing_id') is not None:
                 del context['missing_id']
         except:
